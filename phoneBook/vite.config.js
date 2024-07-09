18e0b7828f72dfd350e093be9d93c8de402c1f89
Artlz21 <darren.lozano01@utrgv.edu>
2024-07-09 15:33:59 -0500

    final update

--- phoneBook/vite.config.js
+++ phoneBook/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
+      '/data': {
         target: 'http://localhost:3001',
         changeOrigin: true,
       }
